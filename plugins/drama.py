3a78f76230080084c2ff4caab5924daaa919e2ce
bigbrother666 <zeming.zhao@gmail.com>
2022-05-29 16:26:20 +0800

    0.1

--- plugins/drama.py
+++ plugins/drama.py
@@ -1,72 +1,238 @@
-""""""
 import os
+import re
+import json
+import time
 from typing import (
-    Optional, List,
+    Dict, Optional, List, Set, Tuple, Union
 )
+import xlrd
+
 from wechaty import (
+    Contact,
+    FileBox,
     MessageType,
     WechatyPlugin,
+    Room,
     Message,
     WechatyPluginOptions
 )
 from wechaty_puppet import get_logger
-from plugins.inspurai.inspurai import Yuan
+from antigen_bot.forward_config import Conversation, ConfigFactory
+from antigen_bot.utils import remove_at_info
+from utils import DFAFilter
+
 
 class DramaPlugin(WechatyPlugin):
     """
-    功能点：应用源1.0进行场景对话模拟
-    授权的群或者我本人中发出的text，直接作为输入
+    AI soul 在 Pyhton-wechat的插件
+    用于Awada长期运营
+    Author：bigbrother666
+    All rights reserved 2022
     """
     def __init__(
-        self,
-        options: Optional[WechatyPluginOptions] = None,
-        admin_id: List[str] = ["wxid_a6xxa7n11u5j22", "wxid_tnv0hd5hj3rs11"]) -> None:
+            self,
+            options: Optional[WechatyPluginOptions] = None,
+            configs: str = 'drama_configs'
+    ) -> None:
 
         super().__init__(options)
-
-        self.cache_dir = '.drama'
-        os.makedirs(self.cache_dir, exist_ok=True)
+        # 1. create the cache_dir
+        self.config_files = configs
+        self.cache_dir = f'{self.config_files}/{self.name}'
+        self.file_cache_dir = f'{self.cache_dir}/file'
+        os.makedirs(self.file_cache_dir, exist_ok=True)
 
         # 2. save the log info into <plugin_name>.log file
-        log_file = os.path.join(self.cache_dir, self.name + '.log')
-        self.logger = get_logger(log_file)
-        
-        # 3. save the admin status
-        self.admin_id = admin_id
-        
-        # 4. cache the Room/Contact data
-        self._rooms: List[str] = []
+        log_file = os.path.join(self.cache_dir, 'log.log')
+        self.logger = get_logger(self.name, log_file)
 
-    async def on_message(self, msg: Message) -> None:
+        # 3. check and load metadata
+        if self._file_check() is False:
+            raise RuntimeError('Drada plugin needs above config_files, pls add and try again')
 
-        if msg.is_self() or msg.type() != MessageType.MESSAGE_TYPE_TEXT:
+        with open(os.path.join(self.config_files, 'directors.json'), 'r', encoding='utf-8') as f:
+            self.directors = json.load(f)
+
+        self.mmrules = self._load_MMrules()
+        if self.mmrule == {}:
+            raise RuntimeError('Drada MMrules.xlsx not valid, pls refer to above info and try again')
+
+        # 4. load self-memory data and create memory-dict for users
+        self.self_memory, self.user_memory = self._load_memory()
+        if self.self_memory == {} or self.user_memory == {}:
+            raise RuntimeError('Drada memory.xlsx not valid, pls refer to above info and try again')
+
+        # 5. test the rasa nlu server and compare the intents and entries with the meta/memory data
+        self.rasa_nlu_server = 'http://localhost:5005/parse'
+
+
+    def _file_check(self) -> bool:
+        """check the config file"""
+        if not os.path.exists(self.config_files):
+            self.logger.warning(f'config file url does not exist! {self.config_files}')
+            return False
+
+        if not os.path.isdir(self.config_files):
+            self.logger.warning(f'config file url is not a directory! {self.config_files}')
+            return False
+
+        if not os.listdir(self.config_files):
+            self.logger.warning(f'config file url is empty:!{self.config_files}')
+            return False
+
+        if "directors.json" not in os.listdir(self.config_files):
+            self.logger.warning(f'config file url does not have directors.json:!{self.config_files}')
+            return False
+
+        if "MMrules.xlsx" not in os.listdir(self.config_files):
+            self.logger.warning(f'config file url does not have MMrules.xlsx:!{self.config_files}')
+            return False
+
+        if "memory.xlsx" not in os.listdir(self.config_files):
+            self.logger.warning(f'config file url does not have memory.xlsx:!{self.config_files}')
+            return False
+
+        if "scenarios.xlsx" not in os.listdir(self.config_files):
+            self.logger.warning(f'config file url does not have scenarios.xlsx:!{self.config_files}')
+            return False
+
+    def _load_memory(self) -> dict:
+        """load the memory data and create memory-dict for users"""
+        memory_file = os.path.join(self.config_files, 'memory.xlsx')
+        data = xlrd.open_workbook(memory_file)
+        table = data.sheets()[0]
+
+        nrows = table.nrows
+        if nrows == 0:
+            self.logger.warning(f'no memory data in {memory_file},this is not allowed')
+            return {},{}
+
+        self_memory = {}
+        user_memory = {}
+
+        for i in range(nrows):
+            k,v = table.row_values(i)
+            if k and v:
+                self_memory[k] = v
+                user_memory[k] = []
+            else:
+                self.logger.warning(f'{k} or {v} is empty in {memory_file}')
+                return {},{}
+
+        return self_memory, user_memory
+
+    def _load_MMrules(self) -> dict:
+        """load the Memory Mathmatics Rules from excel"""
+        MMrules = os.path.join(self.config_files, 'MMrules.xlsx')
+        data = xlrd.open_workbook(scenarios_file)
+        table = data.sheets()[0]
+
+        nrows = table.nrows
+        if nrows == 0:
+            self.logger.warning(f'no memory data in {memory_file},this is not allowed')
+            return {}
+
+        cols = table.ncols
+
+        if table.cell_value(0,1).lower() != 'read' or table.cell_value(0,2).lower() != 'bi':
+            self.logger.warning(f'{MMrules} is not in the right format')
+            return {}
+
+        rules = {}
+        for i in range(1, nrows):
+            for k in range(cols):
+                if k == 0:
+                    if table.cell_value(i,k).isalpha():
+                        intent = table.cell_value(i,k)
+                        rules[intent] = {}
+                    else:
+                        self.logger.warning(f'{MMrules} is not in the right format')
+                        return {}
+                    continue
+                if table.cell_value(i,k).lower() not in ['yes','no']:
+                    self.logger.warning(f'{MMrules} is not in the right format')
+                    return {}
+                else:
+                    rules[intent][table.cell_value(0,k).lower()] = table.cell_value(i,k).lower()
+
+        return rules
+
+
+    def _load_scenarios(self, scenario:str) -> dict:
+        """load the scenarios data as assigned"""
+        scenarios_file = os.path.join(self.config_files, 'scenarios.xlsx')
+        data = xlrd.open_workbook(scenarios_file)
+        table = data.sheet_by_name(scenario)
+
+        nrows = table.nrows
+        if nrows == 0:
+            self.logger.warning(f'no memory data in {memory_file},this is not allowed')
+            return {}
+
+        cols = table.ncols
+
+        for k in range(1, cols):
+            if not table.cell_value(0,k):
+                self.logger.warning(f'{k}th column is empty in {scenarios_file},this is not allowed')
+                return {}
+
+        rules = {}
+        for i in range(1, nrows):
+            for k in range(cols):
+                if k == 0:
+                    if table.cell_value(i,k):
+                        rule = table.cell_value(i,k)
+                        rules[rule] = {}
+                    else:
+                        self.logger.warning(f'cell({k},{v}) is empty in {memory_file},this is not allowed')
+                        return {}
+                    continue
+                rules[rule][table.cell_value(0,k)] = table.cell_value(i,k)
+
+        return rules
+
+
+    async def director_message(self, msg: Message, conversation_id: str):
+        """forward the message to the target conversations
+        Args:
+            msg (Message): the message to forward
+            conversation_id (str): the id of conversation
+        """
+        # 1. get the type of message
+        conversations = self.admin_status.get(conversation_id, [])
+        if not conversations:
             return
 
-        talker = msg.talker()
-        id = talker.contact_id
+        file_box = None
+        if msg.type() in [MessageType.MESSAGE_TYPE_IMAGE, MessageType.MESSAGE_TYPE_VIDEO,
+                          MessageType.MESSAGE_TYPE_ATTACHMENT]:
+            file_box = await msg.to_file_box()
+            file_path = os.path.join(self.file_cache_dir, file_box.name)
 
-        if msg.text() =="ding":
-            await msg.say('dong -- Drama')
-            return
+            await file_box.to_file(file_path, overwrite=True)
+            file_box = FileBox.from_file(file_path)
+#记得读取记忆的时候，如果people的名称本身也是一个entity的话，那么要把相关的记忆也加进来
+        for conversation in conversations:
+            if conversation.type == 'Room':
+                forwarder_target = await self.bot.Room.load(conversation.id)
+            elif conversation.type == 'Contact':
+                forwarder_target = await self.bot.Contact.load(conversation.id)
+            else:
+                continue
 
-        if id in self.admin_id:
-            if msg.room() and await msg.mention_self():
-                if await msg.mention_text() == "觉醒":
-                    await msg.say("演员已就位")
-                    self._rooms.append(msg.room().room_id)
-                    self.logger.info("add rooms"+msg.room().room_id)
-                    return
+            # TODO: 转发图片貌似还是有些问题
+            if file_box:
+                await forwarder_target.say(file_box)
 
-        if msg.room():
-            room = msg.room()
-            if room.room_id not in self._rooms:
-                return
-        else:
-            if id not in self.admin_id:
-                return
+            # 如果是文本的话，是需要单独来转发
+            elif msg.type() == MessageType.MESSAGE_TYPE_TEXT:
+                await forwarder_target.say(msg.text())
 
-        text = msg.text()
-        self.logger.info(text)
+            elif forwarder_target:
+                await msg.forward(forwarder_target)
+
+
+    def soul(self):
 
         yuan = Yuan(input_prefix="",
                     input_suffix="：“",
@@ -87,3 +253,69 @@ class DramaPlugin(WechatyPlugin):
         else:
             await msg.say('something must be wrong')
             self.logger.info('no reply, something goes wrong')
+
+    async def on_message(self, msg: Message) -> None:
+        talker = msg.talker()
+        room: Optional[Room] = msg.room()
+
+        conv: Union[Room, Contact] = room or talker
+
+        # 1. 判断是否是自己发送的消息
+        if talker.contact_id == msg.is_self():
+            return
+
+        # 2. check if is director
+        if not room and talker.contact_id in self.directors:
+            await self.director_message(msg)
+            return
+
+        # 3. message pre-process
+        """
+        1. 是否是文本消息，排除不支持的消息类型（目前只支持文本）
+        2. 预设声明发送（eg首次使用隐私声明）
+        3. 敏感词检测
+        """
+        
+        # 4. check the status of the talker and load the scenario rule-sheet
+
+
+
+        # 5. the soul
+
+
+        text = msg.text()
+        if room:
+            conversation_id = room.room_id
+        else:
+            conversation_id = talker.contact_id
+
+        # at 条件触发
+        if conversation_id not in self.admin_status and self.trigger_with_at:
+            mention_self = await msg.mention_self()
+            if not mention_self:
+                return
+            text = remove_at_info(text=text)
+
+        if conversation_id in self.admin_status:
+            await self.forward_message(msg, conversation_id=conversation_id)
+            self.admin_status.pop(conversation_id)
+            return
+
+        # filter the target conversations
+
+        if text.startswith(self.command_prefix):
+            # parse token & command
+            text = text[len(self.command_prefix):]
+            text = text[text.index('#') + 1:].strip()
+
+            receivers = self.config_factory.get_receivers(conv)
+            if not receivers:
+                return
+
+            self.admin_status[conversation_id] = receivers
+
+            if text:
+                # set the words to the message
+                msg.payload.text = text
+                await self.forward_message(msg, conversation_id=conversation_id)
+                self.admin_status.pop(conversation_id)
---
+++ utils/DFAFilter.py
@@ -0,0 +1,83 @@
+'''
+原作Author guo
+https://github.com/guojia60180/sensitive-words-filter
+改写 bigbrother666
+增加了测试代码，来自https://blog.csdn.net/u013421629/article/details/83178970
+更改了返回模式，返回是否检测到和检测到的敏感词(检测到第一个敏感词就返回）
+'''
+
+class DFAFilter():
+    '''有穷状态机完成'''
+
+    def __init__(self):
+        self.keywords_chains={}
+        self.delimit='\x00'
+
+    def add(self, keyword):
+        keyword=keyword.lower()
+        chars=keyword.strip()
+        if not chars:
+            return
+
+        level = self.keywords_chains
+        for i in range(len(chars)):
+            if chars[i] in level:
+                level = level[chars[i]]
+
+            else:
+                if not isinstance(level,dict):
+                    break
+
+                for j in range(i,len(chars)):
+                    level[chars[j]]={}
+                    last_level,last_char=level,chars[j]
+                    level=level[chars[j]]
+
+                last_level[last_char] = {self.delimit:0}
+                break
+
+        if i == len(chars)-1:
+            level[self.delimit]=0
+
+    def parse(self, path="./utils/keywords"):
+        with open(path, encoding='utf-8') as f:
+            for keyword in f:
+                self.add(keyword.strip())
+
+    def filter(self, message):
+        message = message.lower()
+        start = 0
+        while start < len(message):
+            res = []
+            level = self.keywords_chains
+            for char in message[start:]:
+                if char in level:
+                    if self.delimit not in level[char]:
+                        level = level[char]
+                        res.append(char)
+                    else:
+                        res.append(char)
+                        return ''.join(res)
+            start += 1
+        return
+
+
+if __name__ == "__main__":
+    import time
+    time1 = time.time()
+    gfw = DFAFilter()
+    gfw.parse()
+    print("====敏感词测试====")
+
+    while (1):
+        print("输入Q退出")
+        prompt = input("输入待测文本：")
+        if prompt.lower() == "q":
+            break
+        result = gfw.filter(prompt)
+        if result:
+            print("检测出敏感词：", result)
+        else:
+            print("未查出敏感词")
+        time2 = time.time()
+        print('总共耗时：' + str(time2 - time1) + 's')
